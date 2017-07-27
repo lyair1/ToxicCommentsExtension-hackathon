bb22002ecb7cfd4b7e5ec05e1b883b537e231478
Yair Levi <lyair1@gmail.com>
2017-07-27 14:52:25 -0700

    cleaning

--- logic.js
+++ logic.js
@@ -1,41 +1,41 @@
+function cssHideText(ele, imageUrl){
+    ele.css("text-indent","-9999px");
+    ele.css("background-image", "url(" + imageUrl + ")");
+    ele.css("background-size", "contain");
+    ele.css("cursor", "hand");
+}
+
+function cssShowText(ele){
+    ele.css("text-indent","0px");
+    ele.css("background-image", "none");
+}
+
 function hideToxicComments(commentSpan){
     if(commentSpan.attr('id') != "1"){
-            var toxicImageUrl = chrome.extension.getURL("images/toxic-comment.png");
             commentSpan.attr("id","1");
             console.log("sending request to 'comments' server");
             $.ajax({
                 method: 'POST',
                 url:"https://hateblockapi.azurewebsites.net/api/Toxicity?code=1Rfug4qf3Ra8Uos7F7kZR2NMpYNNGS4B5hiJPp/5HutMsMGHD9893g==",
-                dataType: 'json', // Notice! JSONP <-- P (lowercase)
+                dataType: 'json',
                 crossDomain : true,
                 data: commentSpan.text(),
                 success:function(str){
-                    // do stuff with json (in this case an array)
-
                     var score = parseFloat(str);
                     var isToxic = score < 2; 
                     if (isToxic){
-
-                        commentSpan.css("text-indent","-9999px");
-                        var img = $(document.createElement('img'));
-                        img.attr('src', toxicImageUrl);
-                        commentSpan.css("background-image", "url(" + toxicImageUrl + ")");
-                        commentSpan.css("background-size", "contain");
-
+                        var toxicImageUrl = chrome.extension.getURL("images/toxic-comment.png");
+                        console.log("Comment is toxic");
+                        cssHideText(commentSpan, toxicImageUrl);
                         commentSpan.hover(function(){
-                            commentSpan.css("text-indent","0px");
-                            commentSpan.css("background-image", "none");
+                            cssShowText(commentSpan, toxicImageUrl);                  
                         }, function(){
-                            commentSpan.css("text-indent","-9999px");
-                            var img = $(document.createElement('img'));
-                            img.attr('src', toxicImageUrl);
-                            commentSpan.css("background-image", "url(" + toxicImageUrl + ")");
-                            commentSpan.css("background-size", "contain");
+                            cssHideText(commentSpan);
                         });
                     }else{
-                        // This is a non toxic comment, don't touch it
+                        // This is a non toxic comment, don't change it
+                        console.log("Comment is non toxic");
                     }
-
                 },
                 error:function(XMLHttpRequest, textStatus, errorThrown){
                     console.log("we got an error back from the server." + JSON.stringify(errorThrown));
