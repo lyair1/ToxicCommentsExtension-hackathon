6b549dfe6a5702d025cc94ab6be103f64c8dd9bf
Yair Levi <lyair1@gmail.com>
2017-07-27 17:02:07 -0700

    adding twitter support

---
+++ twitter.js
@@ -0,0 +1,31 @@
+
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-73159092-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+const youtubeCommentFilter = function(node) {
+    hideToxicComments("p.tweet-text");
+};
+
+const observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        mutation.addedNodes.forEach(function(node) {
+            if (node.nodeType === 1) { // ELEMENT_NODE
+                youtubeCommentFilter(node);
+            }
+        });
+    });
+});
+
+const config = { attributes: false, childList: true, characterData: false, subtree: true }
+
+observer.observe(document.body, config);
+
+youtubeCommentFilter(document.body);
